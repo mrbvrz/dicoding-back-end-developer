ca36817b9c203829e3ea46623b2636ea3bb9fe8a
Hasan Suryaman <hasan.suryaman@gmail.com>
2023-05-02 11:36:27 +0700

    change port

--- belajar-membuat-aplikasi-backend-untuk-pemula/bookshelf-api/src/server.js
+++ belajar-membuat-aplikasi-backend-untuk-pemula/bookshelf-api/src/server.js
@@ -2,7 +2,7 @@ const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const server = Hapi.server({
-    port: 3000,
+    port: 5000,
     host: 'localhost',
     routes: {
         cors: {
